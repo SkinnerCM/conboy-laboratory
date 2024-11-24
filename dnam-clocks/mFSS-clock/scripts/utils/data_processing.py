8a60b9d963626475967fc49e2f741a0028df9715
cmskinner2015 <42685434+cmskinner2015@users.noreply.github.com>
2024-11-23 16:36:50 -0800

    update data_processing script

--- dnam-clocks/mFSS-clock/scripts/utils/data_processing.py
+++ dnam-clocks/mFSS-clock/scripts/utils/data_processing.py
@@ -22,15 +22,12 @@
                 - `prep_model`: Prepares a model DataFrame for feature-based predictions.
                 - `list_writer`: Writes a list to a CSV file.
                 - `list_reader`: Reads a list from a CSV file.
-                - `cohens_d`: Computes Cohen's d for effect size estimation.
 
 
  Notes:         - `prep_model` assumes the input DataFrame includes columns with CpG names 
                   and weights. It renames, sorts, and prepares the model for use in prediction tasks.
                 - The `list_writer` and `list_reader` functions are designed for lightweight CSV 
                   file operations and assume a single-row list structure.
-                - The `cohens_d` function calculates effect size between two groups and is 
-                  particularly useful for statistical comparisons in experimental studies.
                 - While the script is tailored to aging research, the functions are modular 
                   and can be adapted for broader use cases with minimal modifications.
                 - Ensure input data conforms to the expected structure for seamless integration.
@@ -82,19 +79,3 @@ def list_reader(file,  encoding="utf8"):
     data = data[0]
 
     return data
-
-def cohens_d(s1, s2):
-    
-    import statistics
-    
-
-    diff = s2.mean()-s1.mean()
-    
-    n1 = len(s1)
-    n2 = len(s2)
-    sd1 = statistics.stdev(s1)
-    sd2 = statistics.stdev(s2)
-    
-    pooled_sd = np.sqrt(((n1-1)*sd1**2+(n2-1)*sd2**2)/(n1+n2-2))
-    
-    return diff/pooled_sd
