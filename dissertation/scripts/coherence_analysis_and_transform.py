c3112ddf8c508a73f3b2e42f4126d376399b2636
cmskinner2015 <42685434+cmskinner2015@users.noreply.github.com>
2024-12-04 11:50:10 -0800

    add coherence analysis and figure plots scripts to dissertation repo

---
+++ dissertation/scripts/coherence_analysis_and_transform.py
@@ -0,0 +1,174 @@
+
+"""
+---------------------------------------------------------------------------------------------------
+ Author: Colin M. Skinner
+ Date Created: 2024-08-02
+ Last Modified: 2024-11-25
+
+ Description:   This script analyzes DNA methylation data to assess shifts in CpG site methylation 
+ 				patterns between healthy and diseased states. It calculates coherence measures 
+ 				based on the relationship between CpG shifts and model weights, offering insights 
+				into biological relevance and consistency across conditions. The script includes 
+ 				functions for regression analysis, distance computation, and data transformation.
+
+ Dependencies:  - Python 3.8
+                - scikit-learn: For machine learning model and metrics
+                - SciPy: For statistical analysis (calculating correlation coefficients)
+                - pandas (assumed): For handling data in DataFrame format
+                - numpy : For handling data in array format
+
+ Usage:        The script assumes pre-processed methylation data and metadata, structured 
+ 			   to facilitate regression analyses and coherence computation. Example usage 
+    		   would involve calling `get_shifts()` or `get_coherence()` with appropriate 
+               inputs derived from experimental datasets   
+
+ Notes:        - Ensure metadata and dataset align in terms of indices and dimensions.
+    		   - Results are sensitive to data quality; preprocessing and validation are 
+     		     crucial for meaningful outcomes.
+    		   - This script is designed for research purposes and is not optimized for 
+    			 high-performance computing.
+ ---------------------------------------------------------------------------------------------------
+"""
+
+import pandas as pd
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from scipy import stats
+
+
+def get_line(params):
+    
+    x_vals = np.linspace(0,99,100,dtype=int)
+    
+    line = [(x,params[0]*x+params[1]) for x in x_vals]
+    
+    return line    
+
+
+def calculate_distance(line1, line2):
+    """
+    Calculate the average distance between corresponding points on two lines.
+    
+    Parameters:
+    line1 (list of tuples): List of (x, y) coordinates representing the first line.
+    line2 (list of tuples): List of (x, y) coordinates representing the second line.
+    
+    Returns:
+    float: Average distance between corresponding points on the two lines.
+    """
+    # Ensure the lines have the same number of points
+    assert len(line1) == len(line2), "Lines must have the same number of points"
+    
+    # Calculate the distances between corresponding points
+    distances = [p1[1] - p2[1] for p1, p2 in zip(line1, line2)]
+    
+    # Calculate the average distance
+    average_distance = np.mean(distances)
+    
+    return average_distance
+
+def get_shifts(data, meta, cg_set, healthy_tag):    
+
+    up_shift=[]
+    down_shift=[]
+
+    healthy = meta.disease_state==healthy_tag
+    disease = meta.disease_state!=healthy_tag
+
+
+    meta_h = meta[healthy]
+    meta_d = meta[disease]
+    data_h = data[healthy]
+    data_d = data[disease]
+
+    for i, cg in enumerate(cg_set):
+
+        h_regression = stats.linregress(meta_h.age, data_h[cg].tolist())
+        h_slope, h_intercept, h_rvalue, pvalue, h_stderr = h_regression
+        
+        h_line = get_line((h_slope,h_intercept))
+
+        d_regression = stats.linregress(meta_d.age, data_d[cg].tolist())
+        d_slope, d_intercept, d_rvalue, pvalue, d_stderr = d_regression
+        
+        d_line = get_line((d_slope,d_intercept))
+        
+        d = conditioned_cohens(h_line, d_line, h_stderr, d_stderr)
+
+        avg_dist = calculate_distance(d_line,h_line)
+
+
+        if avg_dist>0:
+            up_shift+=[(cg, avg_dist, h_rvalue, d, 1,i)]            
+        else:
+            down_shift+=[(cg, avg_dist, h_rvalue, d, -1,i)]
+      
+    up_shift = pd.DataFrame(up_shift, columns=['CpG', 'Shift', 'Correlation','Cohens d','Sign','Order'])
+    up_shift.reset_index(inplace=True,drop=True)
+#     up_shift = up_shift.sort_values(by='Shift', ascending=False, ignore_index=True)
+    
+    down_shift = pd.DataFrame(down_shift, columns=['CpG', 'Shift', 'Correlation','Cohens d','Sign', 'Order'])
+    down_shift.reset_index(inplace=True,drop=True)
+#     down_shift = down_shift.sort_values(by='Shift', ascending=True, ignore_index=True)
+                
+    return up_shift, down_shift
+
+
+
+def get_coherence(dataset, meta, model, healthy_tag):
+    
+    up_shift, down_shift = get_shifts(dataset, meta, model.CpG, healthy_tag)
+    
+    down_shift['Weight'] = model[model['CpG'].isin(down_shift.CpG)].Weight.tolist()
+    down_shift['Coherence'] = (down_shift.Shift*down_shift.Weight)>0
+    
+    up_shift['Weight'] = model[model['CpG'].isin(up_shift.CpG)].Weight.tolist()
+    up_shift['Coherence'] = (up_shift.Shift*up_shift.Weight)>0
+    
+    
+    total_coherence = pd.concat([up_shift, down_shift], ignore_index=True)
+    
+    print('Down-shifted coherence: :', down_shift.Coherence.sum()/len(down_shift))
+    print('Down-shifted mean weight: ', abs(down_shift.Weight).mean())
+    print('Up-shifted coherence: :', up_shift.Coherence.sum()/len(up_shift))
+    print('Up-shifted mean weight: ', abs(up_shift.Weight).mean())
+    print('Total coherence: ', (down_shift.Coherence.sum()+up_shift.Coherence.sum())/len(model.CpG))
+    print('Mean coherence weight: ', abs(total_coherence[total_coherence.Coherence==True].Weight).mean())
+    print('Max coherence weight: ', abs(total_coherence[total_coherence.Coherence==True].Weight).max())
+    print('Mean incoherence weight: ', abs(total_coherence[total_coherence.Coherence!=True].Weight).mean())
+    print('Max incoherence weight: ', abs(total_coherence[total_coherence.Coherence!=True].Weight).max())
+    
+    return total_coherence    
+
+def coherence_transform(data, cg_set):
+    
+    transformed_df = data[cg_set.CpG.tolist()].copy()
+    
+    for i, cg in enumerate(cg_set.CpG.tolist()):
+        
+        if cg_set.Shift[i]<0:
+            transformed_df[cg] = 1 - transformed_df[cg]
+            
+    return transformed_df
+
+
+def conditioned_cohens(line1, line2, sd1, sd2):
+    
+    """This function calculates the effect size between two regression lines. Takes in ordered
+       lists for the y-values of the regression lines, as well as the individual standard errors for the 
+       two regressions
+    """
+    
+    import statistics
+    
+
+    diff = calculate_distance(line1, line2)
+    
+    n1 = len(line1)
+    n2 = len(line2)
+    
+    pooled_sd = np.sqrt(((n1-1)*sd1+(n2-1)*sd2)/(n1+n2-2))
+    
+    return diff/pooled_sd
---
+++ dissertation/scripts/figure_plots.py
@@ -0,0 +1,339 @@
+"""
+--------------------------------------------------------------------------------------------------------------
+ File Name:     figure_plots.py
+ Author:        Colin M. Skinner
+ Date Created:  2024-08-02
+ Last Modified: 2024-11-30
+
+ Description:   This script provides functions for generating and visualizing statistical plots to evaluate machine learning 
+                model performance on age prediction tasks. The plots include scatter plots, residual analyses, and comparisons 
+                of predictions between control and disease groups, with annotated statistical significance.
+
+ Dependencies:  - Python 3.8
+                - seaborn
+                - matplotlib
+                - scipy
+                - pandas
+                - sklearn (for metrics)
+
+ Usage:         - The `fig3_plots` function generates plots for a given model and dataset, focusing on actual vs. predicted age 
+                  comparisons, residual distributions, and boxplots by disease condition.
+                - The `fig4_plots` function offers extended visualization and analysis for feature-refictified coherence models.
+
+ Notes:         - Ensure proper preprocessing of input data before calling these functions.
+                - Customize disease condition names, color palettes, and statistical methods as required.
+ -------------------------------------------------------------------------------------------------------------
+"""
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+from sklearn import metrics
+from utils.stats_utils import cohens_d
+from coherence_analysis_and_transform import coherence_transform
+
+def fig3_plots(df,df_meta, model, model_selection, d_condition, flag=False):
+    
+    """
+    Generates a series of plots for a machine learning model's predictions 
+    against actual values, including scatter plots, residual distributions, 
+    and boxplots, while calculating statistical significance and effect sizes.
+
+    Parameters:
+    -----------
+    df : pandas.DataFrame
+        Input data containing features used by the model.
+    df_meta : pandas.DataFrame
+        Metadata associated with the dataset, including age and health condition.
+    model : object
+        A machine learning model that implements a `predict` method for inference.
+    model_selection : list
+        A list of feature names used for predictions by the model.
+    d_condition : str
+        Name of the disease condition for comparison against a control group.
+    flag : bool, optional
+        If `True`, uses `simple_disease_state` for coloring in the plots; 
+        otherwise, uses `disease_state`. Default is `False`.
+
+    Outputs:
+    --------
+    - Scatter plot of actual age vs. predicted age, with linear regression stats.
+    - KDE plot of residuals, annotated with effect size and p-value.
+    - Boxplot of residuals by health condition, annotated with significance stats.
+
+    Calculations:
+    -------------
+    - Computes regression statistics for predicted vs. actual age.
+    - Calculates residuals (predicted - actual) for each sample.
+    - Performs Welch's t-test and computes Cohen's d effect size between groups.
+
+    Notes:
+    ------
+    - Residuals are plotted for both control and disease conditions.
+    - Visuals use customized color palettes and ordering of categories.
+    """
+    
+    if flag:
+        hue_val = df_meta.simple_disease_state
+    else:
+        hue_val = df_meta.disease_state
+
+    preds = model.predict(df[model_selection])
+    df_meta['model preds'] = preds
+
+    regression = stats.linregress(df_meta.age, preds)
+    slope, intercept, rvalue, pvalue, stderr = regression
+
+    mae = metrics.mean_absolute_error(df_meta.age, preds)
+
+
+    conditions = ['Control', d_condition]
+    # Define the desired order of categories
+    category_order = [conditions[0], conditions[1]]
+
+    blue = sns.color_palette()[0]
+    red=sns.color_palette()[3]
+
+    custom_palette = {conditions[0]:blue, conditions[1]:red}
+
+    # Create a scatter plot of the transformed data
+    plt.figure(figsize=(10, 8)) 
+    
+    sns.scatterplot(x=df_meta.age, y=preds, hue = hue_val, 
+                    palette=custom_palette, hue_order=category_order,
+                    alpha=0.75, s=150, edgecolor="k")
+    
+    vals = np.linspace(1,100,100)
+    sns.lineplot(x=vals, y=vals,linewidth=3, color='k',linestyle='--')
+    plt.xlabel('Actual age (yrs)',fontsize=30)
+    plt.ylabel('Predicted',fontsize=30)
+
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
+    plt.legend(loc='best', fontsize='xx-large')
+
+    plt.title('{}: r={:.2f}, p={:.1g}, MAE={:.1f} yrs'.format(df_meta.series_id[0], rvalue, pvalue, mae),fontsize=20)
+        
+    
+
+    
+    #create kdeplot
+    df_meta['Residuals'] = df_meta['model preds'] - df_meta.age
+    
+    from scipy.stats import ttest_ind
+    h_resids = df_meta[hue_val=='Control'].Residuals
+    d_resids = df_meta[hue_val!='Control'].Residuals
+    # Perform the Welch's t-test (indfendent t-test with unequal variances)
+    statistic, p_value = ttest_ind(h_resids, d_resids, equal_var=False)
+    effect_size =  cohens_d(h_resids, d_resids)
+
+    plt.figure(figsize=(10, 8)) 
+
+
+    sns.histplot(
+            data=df_meta,
+            x='Residuals',
+            hue=hue_val,
+            palette=custom_palette,
+            bins=20,
+            kde=True,
+            alpha=0.4,
+            element="step",
+            legend=False,
+            stat = 'density',
+            common_norm=False,
+            line_kws={'linewidth': 4}
+            )
+    plt.xlabel('Residual (yrs)', fontsize=28)
+    plt.xticks(fontsize=24)
+    plt.ylabel('Density', fontsize=28)
+    plt.yticks(fontsize=24)
+
+    # Plot means for each cohort (healthy status)
+    mean_healthy = h_resids.mean()
+    mean_unhealthy = d_resids.mean()
+
+    plt.axvline(x=mean_healthy, color=blue, linestyle='--', linewidth=3, 
+               ymin=0.01, ymax=0.95, dashes=(4.5, 4.28), zorder=6)
+    plt.axvline(x=mean_healthy, color='k', linestyle='--', linewidth=5, ymax=0.95)
+
+    plt.axvline(x=mean_unhealthy, color=red, linestyle='--', linewidth=3, 
+                 ymin=0.01, ymax=0.95, dashes=(4.5, 4.28), zorder=6)
+    plt.axvline(x=mean_unhealthy, color='k', linestyle='--', linewidth=5, ymax=0.95)
+
+    plt.text(0.65, 0.95, f"d={effect_size:.2f}", fontsize=26, verticalalignment='top', 
+         horizontalalignment='left', transform=plt.gca().transAxes);
+    plt.text(0.65, 0.85, f"p={p_value:.1e}", fontsize=26,  verticalalignment='top', 
+         horizontalalignment='left', transform=plt.gca().transAxes);
+    
+
+
+    #Create boxplot
+    plt.figure(figsize=(10, 8)) 
+    
+    sns.boxplot(data=df_meta, x=hue_val, y='Residuals', palette=custom_palette, order=category_order,linewidth=3)
+    # Add significance bar and p-value
+
+    max_val = max(max(h_resids),max(d_resids))
+    min_val = min(min(h_resids),min(d_resids))
+
+    plt.plot([0, 1], [max_val+2, max_val+2], 'k-', lw=2)
+    plt.text(0.5, max_val+3, f'd = {effect_size:.2g} \n p = {p_value:.2g}', ha='center', fontsize=20)
+
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
+    plt.ylim([min_val-5, max_val+15])
+
+    plt.xlabel(None)
+    plt.ylabel('Residuals (yrs)',fontsize=30)
+
+
+
+def fig4_plots(df,df_meta, model, mfss_selection,coherence_shift, d_condition, flag=False):
+
+    """
+    Generate a series of plots for visualizing model predictions, residuals, and statistical analysis on age predictions.
+
+    This function creates three plots:
+    1. A scatter plot comparing actual vs predicted ages with regression statistics.
+    2. A histogram of residuals for the healthy vs diseased cohorts, with density estimation and statistical annotations.
+    3. A boxplot of residuals for healthy vs diseased cohorts with statistical significance indicated.
+
+    Parameters:
+    -----------
+    df : pd.DataFrame
+        The data frame containing the features used for prediction.
+    df_meta : pd.DataFrame
+        The metadata frame containing age, disease state, and other relevant information.
+    mfss_selection : list
+        The list of feature selection columns to use in the model prediction.
+    d_condition : str
+        The disease state to compare with the control group.
+    flag : bool, optional, default=False
+        Whether to use a simple disease state for the hue in the plot.
+
+    Returns:
+    --------
+    None
+        The function generates and displays the following plots:
+        1. Scatter plot of actual vs predicted age.
+        2. Histogram plot of residuals with statistical annotations.
+        3. Boxplot of residuals with significance line.
+
+    Notes:
+    ------
+    - The scatter plot includes a line of identity and regression statistics.
+    - The histogram plot includes KDEs and calculates effect size (Cohen's d) and p-value from a Welch's t-test.
+    - The boxplot compares the residuals of healthy vs diseased cohorts.
+    """
+    
+    if flag:
+        hue_val = df_meta.simple_disease_state
+    else:
+        hue_val = df_meta.disease_state
+    
+    df_transform = coherence_transform(df, coherence_shift)
+    df_preds = model.predict(df_transform[mfss_selection])
+    df_meta['mfss preds'] = df_preds
+
+    regression = stats.linregress(df_meta[df_meta.disease==0].age, df_preds[df_meta.disease==0])
+    slope, intercept, rvalue, pvalue, stderr = regression
+
+    mae = metrics.mean_absolute_error(df_meta.age, df_preds)
+
+
+    conditions = ['Control', d_condition]
+     # Define the desired order of categories
+    category_order = [conditions[0], conditions[1]]
+
+    blue = sns.color_palette()[0]
+    red=sns.color_palette()[3]
+
+    custom_palette = {conditions[0]:blue, conditions[1]:red}
+
+
+    plt.figure(figsize=(10, 8)) 
+    # Create a scatter plot of the transformed data
+    sns.scatterplot(x=df_meta.age, y=df_preds, hue = hue_val, palette=custom_palette, hue_order=category_order, alpha=0.75, s=150, edgecolor="k")
+    vals = np.linspace(1,100,100)
+    sns.lineplot(x=vals, y=vals,linewidth=3, color='k',linestyle='--')
+    plt.xlabel('Actual age (yrs)',fontsize=30)
+    plt.ylabel('Predicted',fontsize=30)
+
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
+    plt.legend(loc='best', fontsize='xx-large')
+
+    plt.title('{}: Control r={:.2f}, p={:.1g}, Control MAE={:.1f} yrs'.format(df_meta.series_id[0], rvalue, pvalue, mae),fontsize=20)
+
+    
+    df_meta['Residuals'] = df_meta['mfss preds'] - df_meta.age
+
+    from scipy.stats import ttest_ind
+    h_resids = df_meta[hue_val=='Control'].Residuals
+    d_resids = df_meta[hue_val!='Control'].Residuals
+    # Perform the Welch's t-test (indfendent t-test with unequal variances)
+    statistic, p_value = ttest_ind(h_resids, d_resids, equal_var=False)
+    effect_size =  cohens_d(h_resids, d_resids)
+
+    plt.figure(figsize=(10, 8)) 
+
+   
+
+    sns.histplot(
+            data=df_meta,
+            x='Residuals',
+            hue=hue_val,
+            palette=custom_palette,
+            bins=20,
+            kde=True,
+            alpha=0.4,
+            element="step",
+            legend=False,
+            stat = 'density',
+            common_norm=False,
+            line_kws={'linewidth': 4}
+            )
+    plt.xlabel('Residual (yrs)', fontsize=28)
+    plt.xticks(fontsize=24)
+    plt.ylabel('Density', fontsize=28)
+    plt.yticks(fontsize=24)
+
+    # Plot means for each cohort (healthy status)
+    mean_healthy = h_resids.mean()
+    mean_unhealthy = d_resids.mean()
+
+    plt.axvline(x=mean_healthy, color=blue, linestyle='--', linewidth=3, 
+               ymin=0.01, ymax=0.95, dashes=(4.5, 4.28), zorder=6)
+    plt.axvline(x=mean_healthy, color='k', linestyle='--', linewidth=5, ymax=0.95)
+
+    plt.axvline(x=mean_unhealthy, color=red, linestyle='--', linewidth=3, 
+                 ymin=0.01, ymax=0.95, dashes=(4.5, 4.28), zorder=6)
+    plt.axvline(x=mean_unhealthy, color='k', linestyle='--', linewidth=5, ymax=0.95)
+
+    plt.text(0.65, 0.95, f"d={effect_size:.2f}", fontsize=26, verticalalignment='top', 
+         horizontalalignment='left', transform=plt.gca().transAxes);
+    plt.text(0.65, 0.85, f"p={p_value:.1e}", fontsize=26,  verticalalignment='top', 
+         horizontalalignment='left', transform=plt.gca().transAxes);
+
+
+
+    plt.figure(figsize=(10, 8)) 
+    
+    sns.boxplot(data=df_meta, x=hue_val, y='Residuals', palette=custom_palette, order=category_order,linewidth=3)
+    # Add significance bar and p-value
+
+    max_val = max(max(h_resids),max(d_resids))
+    min_val = min(min(h_resids),min(d_resids))
+
+    plt.plot([0, 1], [max_val+2, max_val+2], 'k-', lw=2)
+    plt.text(0.5, max_val+3, f'd = {effect_size:.2g} \n p = {p_value:.2g}', ha='center', fontsize=20)
+
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
+    plt.ylim([min_val-5, max_val+15])
+
+    plt.xlabel(None)
+    plt.title('{}'.format(df_meta.series_id[0], rvalue, pvalue, mae),fontsize=20)
+    plt.ylabel('Residuals (yrs)',fontsize=30)
